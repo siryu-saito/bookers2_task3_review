c04ffcb66686fd386671e6abf7b7fd647391b5e7
siryu-saito <saito.s.ryu@gmail.com>
2021-06-12 14:00:54 +0000

    コメント機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_062142) do
+ActiveRecord::Schema.define(version: 2021_06_12_123523) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
